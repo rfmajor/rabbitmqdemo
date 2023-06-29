3c841621550d0b6705099d9a222d75ed81749a31
fmajor <fmajor@griddynamics.com>
2023-06-29 14:13:26 +0200

    Delete import

--- listener/src/main/java/com/rfmajor/rabbitmqdemo/listener/BeanConfig.java
+++ listener/src/main/java/com/rfmajor/rabbitmqdemo/listener/BeanConfig.java
@@ -1,7 +1,6 @@
 package com.rfmajor.rabbitmqdemo.listener;
 
 import com.rfmajor.rabbitmqdemo.common.Utils;
-import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.context.annotation.Bean;
